e1c415f61ccfeb79e53715874a82a4bc81eb316f
shubconsultadd <shubhankar.d@consultadd.com>
2023-08-10 07:39:02 +0530

    New Files Added

--- Backend/settings.py
+++ Backend/settings.py
@@ -13,7 +13,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import os
-from .secrets import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -181,7 +183,7 @@ CORS_ALLOW_HEADERS = ['Authorization', 'Content-Type']
 
 STORAGES = {"default": {"BACKEND": "storages.backends.s3boto3.S3Boto3Storage"}}
 
-AWS_ACCESS_KEY_ID = AWS_ACCESS_KEY_ID
-AWS_SECRET_ACCESS_KEY = AWS_SECRET_ACCESS_KEY
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'travelplanimages'
 AWS_QUERYSTRING_AUTH = False
